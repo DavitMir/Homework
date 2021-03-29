acb3aad3011cce694b3903e9b9f10dd3755be8cd
DavitMir <81579407+DavitMir@users.noreply.github.com>
2021-03-29 20:01:52 +0400

    Add files via upload

---
+++ JAVA4.java
@@ -0,0 +1,15 @@
+
+public class JAVA4{
+ 	public static void main(String [] args){
+ 	long j=0;
+ 	long i=1;
+
+ 	for(int o = 0;o<50;o++){
+ 		System.out.println(j);
+ 		i = j + i;
+ 		j = i - j;
+ 		
+ 	}
+	}	
+	}	
+ 	
